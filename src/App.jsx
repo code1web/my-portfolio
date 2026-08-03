4c6becaa4a48127cd5f4c63ec31246a21c500620
code1web <vikashchoudhary15616@gmail.com>
2026-08-03 17:08:45 +0530

    Fix router for GitHub Pages blank screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Portfolio from './components/Portfolio';
 import AdminDashboard from './components/Admin/AdminDashboard';
 
